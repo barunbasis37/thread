37bbc798c839942c891448cbe6665b3b02d28f39
Barun Kanti Das <barunbasis37@gmail.com>
2016-06-06 15:32:52 +0600

    Thread Single

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number aNumber=new Number();
+            Thread t=new Thread(aNumber.PrintNumbers);
+            t.Start();
+
+        }
+    }
+
+    class Number
+    {
+        public void PrintNumbers()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
