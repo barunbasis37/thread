382a5e8049d88f920dbd053547b015a554ec9153
Barun Kanti Das <barunbasis37@gmail.com>
2016-06-11 15:02:26 +0600

    Callback Function in Threading

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    public delegate void SumOfNumbersCallBack(int SumOfNumbers);
     class Program
     {
-        static void Main(string[] args)
+        public static void PrintSum(int sum)
+        {
+            Console.WriteLine("Sum of numbers = " + sum);
+        }
+        public static void Main(string[] args)
         {
             
             //Thread t=new Thread(aNumber.PrintNumber);
@@ -17,8 +22,10 @@ namespace ConsoleApplication1
 
             Console.WriteLine("Please Write number");
             int target = Convert.ToInt32(Console.ReadLine());
-            Number aNumber = new Number(target);
-            Thread tParameter=new Thread(aNumber.PrintParameterNumber);
+            SumOfNumbersCallBack callback = new SumOfNumbersCallBack(PrintSum);
+
+            Number aNumber = new Number(target, callback);
+            Thread tParameter=new Thread(aNumber.PrintSumNumber);
             tParameter.Start();
 
         }
@@ -27,21 +34,27 @@ namespace ConsoleApplication1
     class Number
     {
         int _target;
+        private SumOfNumbersCallBack _callBack;
 
-        public Number(int target)
+        public Number(int target, SumOfNumbersCallBack callBack)
         {
             this._target = target;
+            this._callBack = callBack;
         }
-        public  void PrintParameterNumber()
+        public  void PrintSumNumber()
         {
-            //int number = 0;
+            int sum = 0;
             //if (int.TryParse(target.ToString(),out number))
             //{
                 for (int i = 0; i <= _target; i++)
                 {
-                    Console.WriteLine(i);
+                    sum = sum + i;
                 }
             //}
+            if (_callBack != null)
+            {
+                _callBack(sum);
+            }
 
         }
     }
